399b70699a95dc39d7d288484436cfea67c3d18e
Vadim <snooperv@gmail.com>
2022-04-07 16:32:10 +0500

    Задание по HTML

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,7 +4,12 @@
 - age - число
 - gender - 'male' или 'female'
  */
-export function getPersons();
+type TGender = 'male' | 'female';
+export function getPersons(): Array<{
+    name: string;
+    age: number;
+    gender: TGender;
+}>;
 
 /*
 Типизируйте функцию personToString, возвращающую строку и принимающую объект одного из следующих форматов:
@@ -18,4 +23,9 @@ export function getPersons();
    - gender - 'male' или 'female'
    - company- строка
  */
-export function personToString(person);
+export function personToString(person: {
+    name: string;
+    age: number;
+    gender: TGender;
+    company?: string;
+}): string;
